23bae5a1f5e69cacc15661cacc92828bb387c999
Tyler Orndorff <orndorfftyler801@gmail.com>
2021-02-07 16:18:23 -0500

    predeploy update 7

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,7 +5,8 @@ module.exports = {
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
         ? process.env.TEST_DATABASE_URL
-        : process.env.DATABASE_URL
+        : process.env.DATABASE_URL,
+  "ssl": { rejectUnauthorized: false }
 
         //: process.env.DATABASE_URL + '?sslmode=require',
 }
--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const db = knex({
   client: 'pg',
   //connection: DB_URL,
   connection: DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  //ssl: { rejectUnauthorized: false }
 })
 
 app.set('db', db)
