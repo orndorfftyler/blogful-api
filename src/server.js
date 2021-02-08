c03f00fdf5c5435a030aa14f606bf68b2a5f116b
Tyler Orndorff <orndorfftyler801@gmail.com>
2021-02-08 14:33:54 -0500

    package.json update and hardcoded URI

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const { PORT, DATABASE_URL } = require('./config')
 const db = knex({
   client: 'pg',
   //connection: DB_URL,
-  connection: DATABASE_URL,
+  connection: "postgres://wlqbllwezbhhjd:66ddd08a3c84a7bbcb53c0c25b49853a9ac4e0713c668b7baac31e6489412e79@ec2-52-205-3-3.compute-1.amazonaws.com:5432/d5b055a43h20co",
   //ssl: { rejectUnauthorized: false }
 })
 
