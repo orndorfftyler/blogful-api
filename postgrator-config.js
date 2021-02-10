acbb1333a21324f234463d7dc0f6213442482c5c
Tyler Orndorff <orndorfftyler801@gmail.com>
2021-02-10 16:43:10 -0500

    changed order of requires in server

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,16 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-  
   "migrationDirectory": "migrations",
-  "driver": "pg" ,
-  /*
-  "user": 'wlqbllwezbhhjd',
-  "host": 'ec2-52-205-3-3.compute-1.amazonaws.com',
-  "database": 'd5b055a43h20co',
-  "password": '66ddd08a3c84a7bbcb53c0c25b49853a9ac4e0713c668b7baac31e6489412e79',
-  "port": '5432'
-  */
+  "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL+'?ssl=true',
--- src/server.js
+++ src/server.js
@@ -1,27 +1,14 @@
-const knex = require('knex')
-const app = require('./app')
-
-//const { PORT, DB_URL } = require('./config')
 const { PORT, DATABASE_URL } = require('./config')
+const app = require('./app')
+const knex = require('knex')
 
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL+'?ssl=true'
 })
 
-  /*
-  user: 'wlqbllwezbhhjd',
-  host: 'ec2-52-205-3-3.compute-1.amazonaws.com',
-  database: 'd5b055a43h20co',
-  password: '66ddd08a3c84a7bbcb53c0c25b49853a9ac4e0713c668b7baac31e6489412e79',
-  port: '5432'
- */
-  //ssl: { rejectUnauthorized: false }
-
-
 app.set('db', db)
-  
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+  console.log(`Server listening at ${PORT}`)
 })
